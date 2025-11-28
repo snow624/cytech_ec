b5f2c0e0592eaf56ef66dab24f09a9322398b356
“snow624” <snow19980624@gmail.com>
2025-11-28 13:23:54 +0900

    first commit

---
+++ src/main/java/jp/co/sss/cytech_ec/CytechEcApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sss.cytech_ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CytechEcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CytechEcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/sss/cytech_ec/CytechEcApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sss.cytech_ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CytechEcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
